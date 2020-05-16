0b8b5824fdb2cc7409c75e568e2f39b0f2eefc09
Hon <dennis.mejen@gmail.com>
2020-05-16 09:52:00 +0700

    Add react app according to zoom video 00:00:00 - 00:17:00

---
+++ app/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import List from "./pages/blog/List";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/blogs">
+            <List />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ app/src/pages/blog/Show.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Show = () => {
+  return (
+    <div>
+      Show
+    </div>
+  )
+}
+
+export default Show
+Show
