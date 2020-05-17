1beb8c75812e49b6f27f4057916b099ced0cd41a
Hon <dennis.mejen@gmail.com>
2020-05-17 12:11:53 +0700

    Add react app according to zoom video 00:00:00 - 00:12.40

---
+++ app/src/pages/blog/List.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const List = () => {
+
+  const data = [
+    {
+      
+    }
+  ]
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>
+            ID
+          </th>
+          <th>
+            Row
+          </th>
+          <th>
+            createdDate
+          </th>
+        </tr>
+        <tr>
+          <td>
+            1
+          </td>
+          <td>
+            Row
+          </td>
+          <td>
+            createdDate
+          </td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default List;
