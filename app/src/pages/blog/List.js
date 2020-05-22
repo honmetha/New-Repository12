e1f93af4a91e534784c565cc4df3ac720acad189
Hon <dennis.mejen@gmail.com>
2020-05-22 07:48:36 +0700

    Add react app according to zoom video 00:12:40 - 00:16:00

--- app/src/pages/blog/List.js
+++ app/src/pages/blog/List.js
@@ -1,6 +1,22 @@
 import React from "react";
 
-const data = [];
+const data = [
+  {
+    id: 1,
+    name: "Blog 01",
+    createdDate: new Date(),
+  },
+  {
+    id: 2,
+    name: "Blog 02",
+    createdDate: new Date(),
+  },
+  {
+    id: 3,
+    name: "Blog 03",
+    createdDate: new Date(),
+  },
+];
 
 const List = () => {
   return (
@@ -8,14 +24,18 @@ const List = () => {
       <table>
         <tr>
           <th>ID</th>
-          <th>Class</th>
-          <th>Created At</th>
+          <th>Name</th>
+          <th>Created date</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>3/3</td>
-          <td>22 March 2020</td>
-        </tr>
+        {data.map((row) => {
+          return (
+            <tr>
+              <td>{row.id}</td>
+              <td>{row.name}</td>
+              <td>{row.createdDate.toString()}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
