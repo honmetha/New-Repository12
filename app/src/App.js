9e7f8b577253020f55e984f1f1d4f27d71e74364
Hon <dennis.mejen@gmail.com>
2020-05-29 07:52:30 +0700

    Add react app according to zoom video 00:00:00 - 00:17:00

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
+import List from "./pages/blog/List";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/blogs">
+            <List />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
