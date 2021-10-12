3bb6d1d016ce3f7c05dedba76b13d82a3d97975e
Hon <dennis.mejen@gmail.com>
2021-10-12 23:50:45 +0700

    [Added] 7 - Promises

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,25 +1,52 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("error getting resource");
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
   });
-
-  request.open("GET", resource);
-  request.send();
 };
 
-getTodos("todos/luigi.json", (err, data) => {
-  console.log(data);
-  getTodos("todos/mario.json", (err, data) => {
-    console.log(data);
-    getTodos("todos/peach.json", (err, data) => {
-      console.log(data);
-    });
+getTodos("todos/luigi.json")
+  .then((data) => {
+    console.log("promise resolved", data);
+  })
+  .catch((err) => {
+    console.log("promise rejected", err);
   });
-});
+
+// promise example
+
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     // fetch something
+//     resolve("some data");
+//     // reject("some error");
+//   });
+// };
+
+// getSomething().then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
